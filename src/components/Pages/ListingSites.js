3e044e3fc13d93bc21a1301acde80bbb61a637f2
Aurélien Roux <aureroux13@gmail.com>
2019-09-14 16:47:27 -0400

    functional pagination

--- src/components/Pages/ListingSites.js
+++ src/components/Pages/ListingSites.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, CircularProgress, Avatar } from "@material-ui/core";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import ReactPaginate from "react-paginate";
 
 const useStyles = makeStyles({
   avatar: {
@@ -19,15 +20,38 @@ const ListingSites = () => {
   const classes = useStyles();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [itemCount, setItemCount] = useState(0);
+  const [actualPage, setActualPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemPerPage, setItemPerPage] = useState(5);
 
   useEffect(() => {
-    fetch(`https://tracktik-challenge.staffr.com/sites?_page=1&_limit=5`)
+    fetch(
+      `https://tracktik-challenge.staffr.com/sites?_page=${actualPage}&_limit=${itemPerPage}`
+    )
       .then(response => response.json())
       .then(data => setData(data))
       .then(() => setLoading(false));
 
     return () => console.log("unmounting...");
-  }, []);
+  }, [actualPage, itemPerPage]);
+
+  useEffect(() => {
+    fetch(`https://tracktik-challenge.staffr.com/sites`)
+      .then(response => response.json())
+      .then(data => setItemCount(data.length))
+      .then(() => setPageCount(Math.floor(itemCount / itemPerPage)));
+
+    return () => console.log("unmounting...");
+  });
+
+  const handlePageClick = e => {
+    setActualPage(e.selected + 1);
+  };
+
+  const onItemPerPageChange = e => {
+    setItemPerPage(e.target.value);
+  };
 
   return (
     <Container maxWidth="md" className="container listing-sites">
@@ -38,6 +62,25 @@ const ListingSites = () => {
         </div>
       ) : (
         <div>
+          <select onChange={onItemPerPageChange}>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+          </select>
+          <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+          />
           {data.map(({ title, id, address, contacts, images }, index) => {
             return (
               <Link to={`site/${id}`} key={index}>
