46b1c5f234c0766b2ed3ab80e126363ff43b0281
Aurélien Roux <aureroux13@gmail.com>
2019-09-14 11:42:19 -0400

    list clients styles

--- src/components/Pages/ListingClients.js
+++ src/components/Pages/ListingClients.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Container, CircularProgress } from "@material-ui/core";
+import { Container, CircularProgress, Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+
+const useStyles = makeStyles({
+  avatar: {
+    margin: 10
+  },
+  bigAvatar: {
+    margin: 10,
+    width: 60,
+    height: 60
+  }
+});
 
 const ListingClients = () => {
+  const classes = useStyles();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -18,25 +32,40 @@ const ListingClients = () => {
 
   useEffect(() => {
     fetchUrl();
-  }, []);
+  }, [data]);
 
   return (
-    <Container>
+    <Container maxWidth="md" className="listing-clients">
       {loading ? (
-        <div>
-          <CircularProgress />
+        <div className="loader">
+          <CircularProgress size={50} />
         </div>
       ) : (
         <div>
-          {data.map(({ givenName, id }, index) => {
+          {data.map(({ givenName, id, logo, tags }, index) => {
             return (
-              <div key={index}>
-                <Link to={`client/${id}`}>
-                  <div>{givenName}</div>
-                  <div>{id}</div>
-                  <hr></hr>
-                </Link>
-              </div>
+              <Link to={`client/${id}`} key={index}>
+                <div className="client">
+                  <div className="avatar">
+                    <Avatar
+                      alt={`${givenName} logo`}
+                      src={logo}
+                      className={classes.bigAvatar}
+                    />
+                  </div>
+                  <div className="infos">
+                    <div>{givenName}</div>
+                    {tags.map((tag, index) => (
+                      <span className="tag" key={index}>
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
+                  <div className="arrow">
+                    <ArrowForwardIosIcon />
+                  </div>
+                </div>
+              </Link>
             );
           })}
         </div>
