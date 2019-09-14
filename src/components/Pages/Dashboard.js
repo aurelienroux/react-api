2f8c42ec81c4a22b503ede8f660b5562518a2c81
Aurélien Roux <aroux@clever-age.com>
2019-09-13 21:57:09 -0400

    Material dashboard

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Paper, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    justifyContent: "space-around",
+    padding: 50
+  },
+  paper: {
+    padding: theme.spacing(10, 20)
+  }
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Link to="/sites">Sites</Link>
-      <Link to="/clients">Clients</Link>
-    </div>
+    <Container className={classes.container}>
+      <Link to="/sites">
+        <Paper className={classes.paper}>
+          <Typography variant="h4">Sites</Typography>
+        </Paper>
+      </Link>
+      <Link to="/clients">
+        <Paper className={classes.paper}>
+          <Typography variant="h4">Clients</Typography>
+        </Paper>
+      </Link>
+    </Container>
   );
 };
 
