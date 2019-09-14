5c9d42faab16cf3c7388fe4bb2ace29ab9dc4e17
Aurélien Roux <aureroux13@gmail.com>
2019-09-14 14:08:00 -0400

    create client page and styles

--- src/components/Pages/Client.js
+++ src/components/Pages/Client.js
@@ -1,34 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, CircularProgress, Avatar } from "@material-ui/core";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
-class Client extends React.Component {
-  state = {
-    data: "",
-    loading: true
+const useStyles = makeStyles({
+  bigAvatar: {
+    margin: 10,
+    width: 60,
+    height: 60
+  }
+});
+const Client = props => {
+  const classes = useStyles();
+
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  async function fetchUrl() {
+    const response = await fetch(
+      `https://tracktik-challenge.staffr.com/clients?id=${props.match.params.id}`
+    );
+    const json = await response.json();
+
+    setData(json);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchUrl();
+  });
+
+  const goBack = () => {
+    props.history.goBack();
   };
 
-  async componentDidMount() {
-    await fetch(
-      `https://tracktik-challenge.staffr.com/clients?id=${this.props.match.params.id}`
-    )
-      .then(response => response.json())
-      .then(data => this.setState({ data, loading: false }));
-    console.log("data: ", this.state.data);
-  }
-
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        {this.state.loading ? (
-          <div>loading...</div>
-        ) : (
-          <div>
-            <div>{this.state.data[0].givenName}</div>
+  return (
+    <div className="client">
+      {loading ? (
+        <div className="loader">
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className="profile">
+          <div className="header">
+            <Container className="header__container" maxWidth="md">
+              <div onClick={goBack} className="header__arrow">
+                <ArrowBackIosIcon />
+              </div>
+              <Avatar
+                alt={`${data[0].givenName} headquarters`}
+                src={data[0].logo}
+                className={classes.bigAvatar}
+              />
+              <div className="header__infos">
+                <h2 className="header__title">{data[0].givenName}</h2>
+              </div>
+            </Container>
           </div>
-        )}
-      </div>
-    );
-  }
-}
+
+          <Container maxWidth="md">
+            <h3>Specialties</h3>
+            {data[0].tags.map((tag, index) => (
+              <span className="tag" key={index}>
+                {tag}
+              </span>
+            ))}
+          </Container>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Client;
