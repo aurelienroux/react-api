2bf2c845b849baa4b02a5bdafd5720c83d63b89c
Aurélien Roux <aureroux13@gmail.com>
2019-09-14 21:00:02 -0400

    udpate test name

--- src/test/ListingSites.test.js
+++ src/test/ListingSites.test.js
@@ -2,6 +2,6 @@ import React from "react";
 import ListingSites from "../components//Pages/ListingSites";
 import { render } from "@testing-library/react";
 
-it("renders component without crashing without crashing", () => {
+it("renders component without crashing", () => {
   render(<ListingSites />);
 });
